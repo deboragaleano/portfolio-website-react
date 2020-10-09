0d5c2c8cb116f417a8e9f2c4f0d159f0dec33632
deboragaleano <dgaleanoservian@gmail.com>
2020-10-09 19:42:58 +0200

    add material-ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import About from './components/About'; 
 
 function App() {
   return (
     <div className="App">
-      <h1>This will be my portfolio</h1>
+      <h1>This will be my wesbite </h1>
+      <About /> 
     </div>
   );
 }
