61475eaa999b8509619ccf38fed1cd28e63f180b
deboragaleano <dgaleanoservian@gmail.com>
2020-10-10 17:24:08 +0200

    create single cards for projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,27 @@
 import React from 'react'; 
+import {makeStyles} from '@material-ui/styles'; 
 import {Paper, Grid} from '@material-ui/core';
+import SingleProject from './SingleProject'; 
+
+const useStyles = makeStyles({
+    root: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      flexWrap: 'wrap',
+    },
+});
 
 function Projects() {
+    const classes = useStyles();
+
     return (
         <div>
             <h1>Projects</h1>
-            <Grid>
-                <Paper style={{width: 100, height: 100,backgroundColor: '#282c34'}}/>
+            <Grid className={classes.root}>
+                {/* <Paper style={{width: 100, height: 100,backgroundColor: '#282c34'}}/> */}
+                <SingleProject /> 
+                <SingleProject /> 
+                <SingleProject /> 
             </Grid>
         </div>
     )
