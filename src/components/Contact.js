ade08a0bd54e8a27521a7c052bce7af22d02ad2d
deboragaleano <dgaleanoservian@gmail.com>
2020-11-06 14:11:45 +0100

    fix form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,41 +1,59 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles'; 
-import {ReadMore as SendButton} from './About'; 
-import Card from '@material-ui/core/Card';
-import TextField from '@material-ui/core/TextField'; 
-import Container from '@material-ui/core/Container'; 
-
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { ReadMore as SendButton } from "./About";
+import Card from "@material-ui/core/Card";
+import TextField from "@material-ui/core/TextField";
+import Container from "@material-ui/core/Container";
 
 const useStyles = makeStyles({
-    root: {
-        width: '90%',
-        padding: '20px',
-    },
-    btn: {
-        margin: '20px'
-    }
-})
+  root: {
+    width: "90%",
+    padding: "20px",
+  },
+  btn: {
+    margin: "20px",
+  },
+});
 
 export default function Contact() {
-    const classes = useStyles();
-    
-    return (
-        <Container maxWidth='sm'>
-            <h2>CONTACT</h2>
-            <Card className={classes.root}>
-                <form name="contact" method="POST" netifly data-netlify="true">
-                    <TextField id="standard-basic" 
-                        label="name" name="name" /> <br />
-                    <TextField id="standard-basic" 
-                        label="email" name="email" /> <br /> 
-                    <TextField multiline rows={4}
-                        id="standard-multiline-static" 
-                        label="message" name="message" /> <br />
-                    <div className={classes.btn}>   
-                    <SendButton type="submit">Send</SendButton>
-                    </div> 
-                </form>
-            </Card>
-        </Container>
-    )
+  const [success, setSuccess] = useState(false);
+  const classes = useStyles();
+
+  useEffect(() => {
+    if ( window.location.search.includes('success=true') ) {
+      setSuccess(true);
+    }
+  }, []);
+
+  return (
+    <Container maxWidth="sm">
+      <h2>CONTACT</h2>
+      {success && (
+          <p style={{ color: "green" }}>Successfully submitted form!</p>
+        )}
+      <Card className={classes.root}>
+        <form 
+            name="contact" 
+            method="POST" 
+            action="/?success=true"
+            data-netlify="true" 
+            >
+        <input type="hidden" name="form-name" value="contact" />
+          <TextField id="standard-basic" label="name" name="name" /> <br />
+          <TextField id="standard-basic" label="email" name="email" /> <br />
+          <TextField
+            multiline
+            rows={4}
+            id="standard-multiline-static"
+            label="message"
+            name="message"
+          />
+          <br />
+          <div className={classes.btn}>
+            <SendButton type="submit">Send</SendButton>
+          </div>
+        </form>
+      </Card>
+    </Container>
+  );
 }
